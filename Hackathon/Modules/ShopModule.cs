a07035015474c7f133ba4ffed98198135c2966fb
Carson <carson-dickinson@hotmail.com>
2025-03-30 22:30:51 -0500

    layout shop features which need to be impl

--- Hackathon/Modules/ShopModule.cs
+++ Hackathon/Modules/ShopModule.cs
@@ -14,9 +14,11 @@ namespace Hackathon.Modules;
 [Group("shop", "commands for interacting with xolobot")]
 public class ShopModule : ModuleBase
 {
-	public ShopModule(ILogger<ModuleBase> logger, MongoDBService mongoDbService, OpenAIService openAIService, DiscordSocketClient client, InteractionHandler interaction) : base(logger, mongoDbService, openAIService, client, interaction){}
+	public ShopModule(ILogger<ModuleBase> logger, MongoDBService mongoDbService, OpenAIService openAIService, DiscordSocketClient client, InteractionHandler interaction) : base(logger, mongoDbService, openAIService, client, interaction) { }
 
 
+
+	#region GUI INTERACTION
 	[SlashCommand("open", "Opens Shop")]
 	public async Task ShopCommand()
 	{
@@ -25,8 +27,8 @@ public class ShopModule : ModuleBase
 		await FollowupAsync("hmmmmmmmmmmmm");// stops the indefinate "* * * xolobot is thinking..."
 	}
 
-	[SlashCommand("view", "View specifc items")]
-	public async Task ItemInfoCommand(
+	[SlashCommand("view", "same as search")]
+	public async Task ViewCommand(
 		[Summary("query", "items with names and tags containing")]
 		string searchTerm)
 	{
@@ -35,25 +37,64 @@ public class ShopModule : ModuleBase
 		await FollowupAsync("hmmmmmmmmmmmm");// stops the indefinate "* * * xolobot is thinking..."
 	}
 
+	[SlashCommand("search", "search specifc items")]
+	public async Task SearchCommand(
+	[Summary("query", "items with names and tags containing")]
+		string searchTerm)
+	{
+		await DeferAsync();// stops error messages when there isnt an error
+		OpenItemPage(Context.Channel, searchTerm);
+		await FollowupAsync("hmmmmmmmmmmmm");// stops the indefinate "* * * xolobot is thinking..."
+	}
+	#endregion
+
+	#region DIRECT INTERACTION
+	[SlashCommand("buy", "Purchase item(s) directly")]
+	public async Task BuyCommand(
+	[Summary("item", "The name of the item to buy")] string itemName,
+	[Summary("quantity", "The number of items to purchase")] int quantity)
+	{
+
+	}
+
+	[SlashCommand("sell", "Sell item(s) directly")]
+	public async Task SellCommand(
+		[Summary("item", "The name of the item to sell")] string itemName,
+		[Summary("quantity", "The number of items to sell")] int quantity)
+	{
+
+	}
+	#endregion
+
+	#region OTHER
+	[SlashCommand("haggle", "Negotiate the price of an item using AI")]
+	public async Task HaggleCommand(
+		[Summary("item", "The name of the item to haggle for")] string itemName)
+	{
+
+	}
+	#endregion
+
+
+	#region HELPER FUNCTIONS
+
 	private async void OpenShop(ISocketMessageChannel location)
 	{
+		// compact
 		var items = await _database.GetShopItems();
 		await ShopManager.Instance.ShowShopPage(location, 0, items);
 	}
 
 	private async void OpenItemPage(ISocketMessageChannel location, String searchTerm)
 	{
+		// specific
 		var items = await _database.GetShopItems(searchTerm);
 		await ShopManager.Instance.ShowItemPage(location, searchTerm, 0, items, Context.User);
 	}
 
-	// Buy item
-	// Buy is on item view page
+	private void ProcessBuy() { }
+	private void ProcessSell() { }
+	private void ProcessHagle() { }
 
-
-	// etc.
-
-
-
-
+	#endregion
 }
